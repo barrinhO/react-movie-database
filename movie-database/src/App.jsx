386a924b7003a802840c4847447a6ed4965f00d7
Christian - PC_SENAI <chrisgitgg@gmail.com>
2025-05-21 16:19:26 -0300

    Renderizando a API

--- movie-database/src/App.jsx
+++ movie-database/src/App.jsx
@@ -1,6 +1,15 @@
-import "./index.css";
+import React from "react";
+import APIOMDB from "./components/ApiOMDB.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1 className="bg-black text-white">Movie Database</h1>
+        <APIOMDB />
+      </div>
+    </>
+  );
 }
 
 export default App;
