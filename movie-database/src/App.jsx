155e8980ea1d9cba790e41c57d8a3f112cda41f5
Christian - PC_SENAI <chrisgitgg@gmail.com>
2025-05-21 14:43:59 -0300

    Testes

--- movie-database/src/App.jsx
+++ movie-database/src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from "react";
-import Button from "./components/Button";
-
 import "./index.css";
 function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
-      <div>
-        <Button />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
